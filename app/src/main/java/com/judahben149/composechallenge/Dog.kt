a8c2ae1beeee4ebc1ba23a86fb45ed6a982b5391
Judah <judahben149@gmail.com>
2023-03-18 23:30:52 +0100

    Initial commit

---
+++ app/src/main/java/com/judahben149/composechallenge/Dog.kt
@@ -0,0 +1,25 @@
+package com.judahben149.composechallenge
+
+
+data class Dog(
+    val dogName: String,
+    val distance: String,
+    val eta: String,
+    val gender: String,
+    val dogInfo: DogInfo,
+    val ownerInfo: OwnerInfo
+)
+
+data class DogInfo(
+    val age: Double,
+    val color: String,
+    val weight: Double,
+    val imageUrl: String,
+    val description: String
+)
+
+data class OwnerInfo(
+    val name: String,
+    val description: String,
+    val imageUrl: String
+    )
